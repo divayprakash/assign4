001970a160620f51524c5a2b3d4d6153896062d3
Divay Prakash <divay14039@iiitd.ac.in>
2015-08-31 00:48:01 +0530

    Timer implemented to print names

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 //**********Header files**********//
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
 //********************************//
 
 //**********Macro declarations**********//
 #define _MMIO_BYTE(mem_addr) (*(volatile uint8_t *)(mem_addr))
 #define _SFR_IO8(io_addr) _MMIO_BYTE((io_addr) + __SFR_OFFSET)
 #define __SFR_OFFSET 0x20
+
+#define _MMIO_WORD(mem_addr) (*(volatile uint16_t *)(mem_addr))
+#define _SFR_MEM8(mem_addr) _MMIO_BYTE(mem_addr)
+#define _SFR_MEM16(mem_addr) _MMIO_WORD(mem_addr)
 //**************************************//
 
 //**********Register Declarations**********//
@@ -13,6 +19,22 @@
 #define PORTB _SFR_IO8(0x05)
 #define DDRD _SFR_IO8(0x0A)
 #define PORTD _SFR_IO8(0x0B)
+
+#define TCCR1A _SFR_MEM8(0x80)
+#define WGM10 0
+#define WGM11 1
+
+#define TCCR1B _SFR_MEM8(0x81)
+#define CS10 0
+#define CS11 1
+#define CS12 2
+#define WGM12 3
+#define WGM13 4
+
+#define TCNT1 _SFR_MEM16(0x84)
+#define OCR1A _SFR_MEM16(0x88)
+#define TIMSK1 _SFR_MEM8(0x6F)
+#define OCIE1A 1
 //*****************************************//
 
 //**********LCD Functions**********//
@@ -32,6 +54,10 @@
 //Set LCD to 4bit mode
 //*********************************//
 
+//**********Global Variable**********//
+int flag = 0;
+//***********************************//
+
 //**********Function Prototypes**********//
 void initializeLCD (void );
 void writeStringLCD (uint8_t *);
@@ -40,7 +66,29 @@ void writeInstructionLCD (uint8_t );
 void writeToLCD (uint8_t );
 void set4bitModeLCD();
 void funcResetLCD();
+void setup_delay();
 //***************************************//
+
+//**********Function to setup timer**********//
+void setup_delay() 
+{
+	TCCR1A = 0;
+	// set entire TCCR1A register to 0
+  	TCCR1B = 0;
+  	// same for TCCR1B
+  	TCNT1  = 0;
+  	//initialize counter value to 0
+  	// set compare match register for 1hz increments
+  	OCR1A = 31249;
+  	// = (16*10^6) / (2*1024) - 1 (must be <65536)
+  	// turn on CTC mode
+  	TCCR1B |= (1 << WGM12);
+  	// Set CS10 and CS12 bits for 1024 prescaler
+  	TCCR1B |= (1 << CS12) | (1 << CS10);  
+  	// enable timer compare interrupt
+  	TIMSK1 |= (1 << OCIE1A);
+}
+//*******************************************//
 
 //**********Names to display**********//
 uint8_t name1[] = "Divay Prakash";
@@ -99,23 +147,33 @@ void initializeLCD()
 }
 //**********************************************//
 
-//**********Print names**********//
-void print_names()
+//**********Interrupt Service Routine to print names**********//
+ISR(TIMER1_COMPA_vect)
 {
-	writeInstructionLCD(returnToHomeLCD);
-	_delay_ms(4);
-	writeInstructionLCD(clearLCD);
-	_delay_ms(4);
-	writeStringLCD(name1);
-	_delay_ms(2000);
-	writeInstructionLCD(returnToHomeLCD);
-	_delay_ms(4);
-	writeInstructionLCD(clearLCD);
-	_delay_ms(4);
-	writeStringLCD(name2);
-	_delay_ms(2000);
+	if (flag == 0) 
+	{
+		writeInstructionLCD(returnToHomeLCD);
+		_delay_ms(4);
+		writeInstructionLCD(clearLCD);
+		_delay_ms(4);
+		writeStringLCD(name1);
+		_delay_ms(2000);
+		flag = 1;
+	}
+	else 
+	{
+		writeInstructionLCD(returnToHomeLCD);
+		_delay_ms(4);
+		writeInstructionLCD(clearLCD);
+		_delay_ms(4);
+		writeStringLCD(name2);
+		_delay_ms(2000);
+		flag = 0;
+	}
+	TCNT1 = 0;
+  // Reset Timer1 Count Register
 }
-//*******************************//
+//************************************************************//
 
 //**********Write a string to LCD**********//
 void writeStringLCD(uint8_t s[])
@@ -177,12 +235,15 @@ void writeToLCD(uint8_t b)
 //**********Main function**********//
 int main(void)
 {
+	cli();
+	setup_delay();
 	DDRD |= 0b11110000;
 	// Pins 14-11 on LCD (D7-4) to PORTD7-4 on ATMega328 (pin 7-4 on Arduino)
 	DDRB |= 0b00000011;
 	// RS and EN on LCD (4, 6) to PORTB1,0 on ATMega328 (pin 9,8 on Arduino)
-	initializeLCD();							
-	while (1) print_names();		
+	initializeLCD();
+	sei();							
+	while (1) {}		
 	return 0;
 }
 //*********************************//
